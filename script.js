29aa3e6d28ece945762d7d3affb967c411a09fbb
Toni Langley <chibia2@gmail.com>
2016-04-11 08:04:09 -0400

    added js for janimate

---
+++ script.js
@@ -0,0 +1,7 @@
+    $(document).ready(function(){
+
+      //using janimate
+      // $("#title").ready(function(){
+        $("h1").jAnimateSequence(["bounce", "tada", "wobble"]);
+      // });
+    });
