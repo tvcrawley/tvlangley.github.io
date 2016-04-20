9c166f99ffc5e77dc9e0bc185bfc2f893b5178db
Toni Langley <chibia2@gmail.com>
2016-04-20 08:32:14 -0400

    brought html content to index

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-    $(document).ready(function(){
+$(document).ready(function(){
 
-      //using janimate
-      // $("#title").ready(function(){
-        $("h1").jAnimateSequence(["bounce", "tada", "wobble"]);
-      // });
+//smooth scroll to section doesn't work
+  $("a").click(function(evt){
+    evt.preventDefault();
+
+    var hash = this.hash;
+
+    $("html, body").animate({
+      scrollTop: $(hash).offset().top
+    }, 1000, function(){
+      window.location.hash = hash;
     });
+    // return false;
+  })
+  //using janimate
+  // $("#title").ready(function(){
+  // $("h1").jAnimateSequence(["bounce", "tada", "wobble"]);
+  // });
+});
