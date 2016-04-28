b1707d9dc33fea2ae98d3ac945f521efb5deb06b
Toni Langley <chibia2@gmail.com>
2016-04-27 20:36:40 -0400

    improving animated headlines

--- Myjs/script.js
+++ Myjs/script.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
 
   //smooth scroll
+
+  // var headerHeight = $("div#header").height();
+  //
+  // // Attach the click event
+  // $('a[href*=#]').bind("click", function(e) {
+  //
+  //     var target = $(this).attr("href"); //Get the target
+  //     var scrollToPosition = $(target).offset().top - headerHeight;
+  //
+  //     $('html').animate({ 'scrollTop': scrollToPosition }, 600, function(target){
+  //         window.location.hash = target;
+  //     });
+  //
+  // });
+
   $(".scroll").click(function(evt){
     evt.preventDefault();
 
@@ -11,8 +26,7 @@ $(document).ready(function(){
     }, 1000, function(){
       window.location.hash = hash;
     });
-    // return false;
-  })
+  });
 
   $("h3").click(function(){
     $(this).siblings(".description").slideToggle("slow");
