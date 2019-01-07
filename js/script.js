7512440068b55f5bcc12cf15add9a122bd25f03e
tvlangley <chibia2@gmail.com>
2019-01-07 15:01:23 -0500

    Add styling to portfolio
    - Add styling for each project
    - Add functionality for each project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
+
+const toggleDescription = function (event) {
+  // toogle words
+   $(this).children().toggle()
+
+  // toggle image
+  if ($(this).css('background-size') === 'cover') {
+    $(this).css('background-size', '0 0')
+  } else {
+    $(this).css('background-size', 'cover')
+  }
+}
+
+$(document).ready(function () {
   $('.sidenav').sidenav()
+  $('#portfolio .col').hover(toggleDescription)
 })
